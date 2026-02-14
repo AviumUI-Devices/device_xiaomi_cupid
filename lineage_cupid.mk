317a6b4f9996729ce7bd64a2dfa4c5211afa1403
Canary233 <2096276316@qq.com>
2026-02-14 23:30:53 +0800

    cupid: AviumUI bringup

--- lineage_cupid.mk
+++ lineage_cupid.mk
@@ -7,7 +7,7 @@
 # Inherit from cupid device
 $(call inherit-product, device/xiaomi/cupid/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common AviumUI configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_cupid
@@ -15,6 +15,17 @@ PRODUCT_DEVICE := cupid
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2201123G
+
+#AviumUI
+AVIUM_IS_OFFICIAL := true
+AVIUM_MAINTAINER := Canary233
+AVIUM_SETTINGS_SOC_MODEL_NAME := Snapdragon 8Gen1
+AVIUM_SETTINGS_DEVICE_CODENAME := Xiaomi 12
+AVIUM_FORCE_SET_FAKE_PROP := true
+# GMS
+WITH_GMS := true
+# Google Sans
+TARGET_USES_GSANS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="cupid_global-user 15 AQ3A.241006.001 OS2.0.209.0.VLCMIXM release-keys" \
